12c7e3d8ca7b9c2e43f613953341ad45b1d8239c
Ahzam Syed <ahzam183@gmail.com>
2023-05-04 10:05:39 +0530

    Form updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/login.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-regular-svg-icons'
 
 export default function Login() {
+
+    const [signUp, setSignUp] = useState(true)
+
+    const [values, setValues] = useState({
+        password: "",
+        showPassword: false,
+    });
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+    const handlePasswordChange = (prop) => (e) => {
+        setValues({ ...values, [prop]: e.target.value });
+    };
+
     return (
         <>
             <div className='Login-Signup'>
@@ -9,16 +25,71 @@ export default function Login() {
                     <div className='container-left'>
 
                     </div>
-                    <div className='form-container'>
+                    {signUp ?
+                        <div className='form-container'>
+                            <div className='form-box'>
+                                <form action="">
+                                    <input type="text" placeholder='Username' />
+                                    <div className='input-box'>
+                                        <input
+                                            type={values.showPassword ? "text" : "password"}
+                                            onChange={handlePasswordChange("password")}
+                                            placeholder='Password'
+                                            value={values.password}
+                                        />
+                                        <FontAwesomeIcon icon={faEye} onClick={handleClickShowPassword} className='show-hide' />
+                                    </div>
+                                    <button className='btn'>Login</button>
+                                </form>
+                            </div>
+                            <div style={{ textAlign: "center" }} >Don't have any account yet? <a onClick={() => setSignUp(false)}><strong>Sign Up</strong></a></div>
+                        </div> :
+                        <div className='form-container'>
+                            <div className='form-box'>
+                                <form action="">
+                                    <input type="text" placeholder='Username' />
+                                    <div className='input-box'>
+                                        <input
+                                            type={values.showPassword ? "text" : "password"}
+                                            onChange={handlePasswordChange("password")}
+                                            placeholder='Password'
+                                            value={values.password}
+                                        />
+                                        <FontAwesomeIcon icon={faEye} onClick={handleClickShowPassword} className='show-hide' />
+                                    </div>
+                                    <div className='input-box'>
+                                        <input
+                                            type={values.showPassword ? "text" : "password"}
+                                            onChange={handlePasswordChange("password")}
+                                            placeholder='Password'
+                                            value={values.password}
+                                        />
+                                        <FontAwesomeIcon icon={faEye} onClick={handleClickShowPassword} className='show-hide' />
+                                    </div>
+                                    <button className='btn'>Login</button>
+                                </form>
+                            </div>
+                            <div style={{ textAlign: "center" }} >Already have an account? <a onClick={() => setSignUp(true)}><strong>Login</strong></a></div>
+                        </div>
+                    }
+                    {/* <div className='form-container'>
                         <div className='form-box'>
                             <form action="">
                                 <input type="text" placeholder='Username' />
-                                <input type="password" placeholder='Password' />
+                                <div className='input-box'>
+                                    <input
+                                        type={values.showPassword ? "text" : "password"}
+                                        onChange={handlePasswordChange("password")}
+                                        placeholder='Password'
+                                        value={values.password}
+                                    />
+                                    <FontAwesomeIcon icon={faEye} onClick={handleClickShowPassword} className='show-hide' />
+                                </div>
                                 <button className='btn'>Login</button>
                             </form>
                         </div>
-                        <div>Don't have any account yet? <a><strong>Sign Up</strong></a></div>
-                    </div>
+                        <div style={{textAlign: "center"}} >Don't have any account yet? <a><strong>Sign Up</strong></a></div>
+                    </div> */}
                 </div>
             </div>
         </>
