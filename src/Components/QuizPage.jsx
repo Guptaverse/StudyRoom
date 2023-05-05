5b76ab42ccfe1fdafab06d0726ae149cd241d280
Ahzam Syed <ahzam183@gmail.com>
2023-05-05 16:40:12 +0530

    Updated

--- src/Components/QuizPage.jsx
+++ src/Components/QuizPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/quiz.css'
-import { Pagination } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 const quizarr = [
+
     {
         ques: "ques1",
         options: ["optionques1", "optionques1", "optionques1", "optionques1"]
@@ -13,27 +13,32 @@ const quizarr = [
         ques: "ques2",
         options: ["optionques2", "optionques2", "optionques2", "optionques2"]
     },
-    // {
-    //     ques: "ques3",
-    //     options: ["optionques3", "optionques3", "optionques3", "optionques3"]
-    // },
-    // {
-    //     ques: "ques4",
-    //     options: ["optionques4", "optionques4", "optionques4", "optionques4"]
-    // },
-    // {
-    //     ques: "ques5",
-    //     options: ["optionques5", "optionques5", "optionques5", "optionques5"]
-    // }
+    {
+        ques: "ques3",
+        options: ["optionques3", "optionques3", "optionques3", "optionques3"]
+    },
+    {
+        ques: "ques4",
+        options: ["optionques4", "optionques4", "optionques4", "optionques4"]
+    },
+    {
+        ques: "ques5",
+        options: ["optionques5", "optionques5", "optionques5", "optionques5"]
+    }
 ];
 
 export default function QuizPage() {
+    const [page, setPage] = useState(0)
     return (
         <>
             <div className='quiz-section'>
                 <div className='quiz-container'>
                     <FontAwesomeIcon icon={faQuestion} className='q-mark' />
-                    {quizarr.map((quiz) => {
+                    {/* <Carousel afterChange={onChange}> */}
+                    {quizarr.map((quiz, index) => {
+                        if (index != page) {
+                            return null;
+                        }
                         return (
                             <div>
                                 <div className='question'>
@@ -41,32 +46,33 @@ export default function QuizPage() {
                                 </div>
                                 <div className='options'>
                                     <div className='option-container'>
-                                        <div className='option-box'>{quiz.options[0]}</div>
-                                        <div className='option-box'>{quiz.options[1]}</div>
+                                        <div className='option-box' style={{
+                                            marginRight: "15px"
+                                        }}><span className='op-no'>A.</span><span style={{padding: "0 5px"}}>{quiz.options[0]}</span></div>
+                                        <div className='option-box' style={{
+                                            marginLeft: "15px"
+                                        }}><span className='op-no'>B.</span><span style={{padding: "0 5px"}}>{quiz.options[1]}</span></div>
                                     </div>
                                     <div className='option-container'>
-                                        <div className='option-box'>{quiz.options[2]}</div>
-                                        <div className='option-box'>{quiz.options[3]}</div>
+                                        <div className='option-box' style={{
+                                            marginRight: "15px"
+                                        }}><span className='op-no'>C.</span><span style={{padding: "0 5px"}}>{quiz.options[2]}</span></div>
+                                        <div className='option-box' style={{
+                                            marginLeft: "15px"
+                                        }}><span className='op-no'>D.</span><span style={{padding: "0 5px"}}>{quiz.options[3]}</span></div>
                                     </div>
                                 </div>
                             </div>
                         )
                     })}
-                    {/* <div className='question'>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eveniet saepe voluptatibus omnis in. Fuga!</p>
-                    </div>
-                    <div className='options'>
-                        <div className='option-container'>
-                            <div className='option-box'>Option A</div>
-                            <div className='option-box'>Option B</div>
-                        </div>
-                        <div className='option-container'>
-                            <div className='option-box'>Option C</div>
-                            <div className='option-box'>Option D</div>
-                        </div>
-                    </div> */}
                 </div>
-                <Pagination defaultCurrent={1} total={20} />
+                <div className='quiz-btns'>
+                    {
+                        quizarr.map((item, i) => {
+                            return <div onClick={() => setPage(i)} className='btn'>{i + 1}</div>
+                        })
+                    }
+                </div>
             </div>
         </>
     )
