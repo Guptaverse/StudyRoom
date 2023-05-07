a76103a477067433a44c0e913b570891a60c4950
Ahzam Syed <ahzam183@gmail.com>
2023-05-07 11:23:02 +0530

    Updated

--- src/Components/QuizPage.jsx
+++ src/Components/QuizPage.jsx
@@ -7,28 +7,38 @@ const quizarr = [
 
     {
         ques: "Which of the following is the correct name of React.js?",
-        options: ["React", "React.js", "ReactJS", "All of the above"]
+        options: ["React", "React.js", "ReactJS", "All of the above"],
+        opNo: ["A.","B.","C.", "D."]
     },
     {
         ques: "Which of the following are the advantages of React.js?",
-        options: ["optionques2", "optionques2", "optionques2", "optionques2"]
+        options: ["optionques2", "optionques2", "optionques2", "optionques2"],
+        opNo: ["A.","B.","C.", "D."]
     },
     {
         ques: "Which of the following is not a disadvantage of React.js?",
-        options: ["optionques3", "optionques3", "optionques3", "optionques3"]
+        options: ["optionques3", "optionques3", "optionques3", "optionques3"],
+        opNo: ["A.","B.","C.", "D."]
     },
     {
         ques: "Which of the following command is used to install create-react-app?",
-        options: ["optionques4", "optionques4", "optionques4", "optionques4"]
+        options: ["optionques4", "optionques4", "optionques4", "optionques4"],
+        opNo: ["A.","B.","C.", "D."]
     },
     {
         ques: " What of the following is used in React.js to increase performance?",
-        options: ["optionques5", "optionques5", "optionques5", "optionques5"]
+        options: ["optionques5", "optionques5", "optionques5", "optionques5"],
+        opNo: ["A.","B.","C.", "D."]
     }
 ];
 
 export default function QuizPage() {
     const [page, setPage] = useState(0)
+    const arr = Array(quizarr.length).fill(-1)
+    const [ans, setAns] = useState(arr)
+    const [selectedOp, setSelectedOp] = useState(-1)
+
+    // console.log(ans)
 
     return (
         <>
@@ -37,31 +47,33 @@ export default function QuizPage() {
                     <FontAwesomeIcon icon={faQuestion} className='q-mark' />
                     {/* <Carousel afterChange={onChange}> */}
                     {quizarr.map((quiz, index) => {
-                        if (index != page) {
+                        if (index !== page) {
                             return null;
                         }
                         return (
-                            <div>
+                            <div key={index}>
                                 <div className='question'>
                                     <p>{quiz.ques}</p>
                                 </div>
                                 <div className='options'>
-                                    <div className='option-container'>
-                                        <div className='option-box' style={{
-                                            marginRight: "15px"
-                                        }}><span className='op-no'>A.</span><span style={{padding: "0 5px"}}>{quiz.options[0]}</span></div>
-                                        <div className='option-box' style={{
-                                            marginLeft: "15px"
-                                        }}><span className='op-no'>B.</span><span style={{padding: "0 5px"}}>{quiz.options[1]}</span></div>
-                                    </div>
-                                    <div className='option-container'>
-                                        <div className='option-box' style={{
-                                            marginRight: "15px"
-                                        }}><span className='op-no'>C.</span><span style={{padding: "0 5px"}}>{quiz.options[2]}</span></div>
-                                        <div className='option-box' style={{
-                                            marginLeft: "15px"
-                                        }}><span className='op-no'>D.</span><span style={{padding: "0 5px"}}>{quiz.options[3]}</span></div>
-                                    </div>
+                                    {quiz.options.map((option, i) => {
+                                        return <div key={i} onClick={() => {
+                                            let temp = ans;
+                                            temp[index] = i;
+                                            setAns(temp);
+                                            setSelectedOp(i)
+                                            console.log(ans)
+                                            console.log(i, selectedOp)
+                                        }} className={`option-box ${selectedOp === i ? 'selected' : ''}`} ><span className='op-no'>{quiz.opNo[i]}</span><span style={{ padding: "0 5px" }}>{option}</span></div>
+                                    })}
+                                </div>
+                                <div className='next-btn'>
+                                    {page === quizarr.length - 1 &&
+                                        <button className="btn">Submit</button>
+                                    }
+                                    {page < quizarr.length - 1 &&
+                                        <button className="btn" onClick={() => setPage(page + 1)}>Next</button>
+                                    }
                                 </div>
                             </div>
                         )
@@ -70,7 +82,7 @@ export default function QuizPage() {
                 <div className='quiz-btns'>
                     {
                         quizarr.map((item, i) => {
-                            return <div onClick={() => setPage(i)} className='btn'>{i + 1}</div>
+                            return <div key={i} onClick={() => setPage(i)} className={`btn ${page === i ? 'selected' : ''}`}>{i + 1}</div>
                         })
                     }
                 </div>
